efa34d0a7aff139a1983064661d49a1c36ccdb71
AjayMT <ajay.tatachar@gmail.com>
2013-04-06 16:04:16 +0530

    Wrote custom security rules.

--- model.js
+++ model.js
@@ -1,2 +1,33 @@
 Boilerplates = new Meteor.Collection('boilerplates');
 Tags = new Meteor.Collection('tags');
+
+Boilerplates.allow({
+  insert: function (userId, doc) {
+    var good = false;
+    Meteor.call('verifyBoilerplate', doc, function (err, res) {
+      if (doc.name && doc.cloneURL && res.name && res.cloneURL && res.userId) good = true;
+    });
+    return good;
+  },
+  remove: function (userId, doc) {
+    return userId === doc.user;
+  },
+  update: function (userId, doc, fields, modifier) {
+    return userId === doc.user;
+  }
+});
+
+Tags.allow({
+  insert: function (userId, doc) {
+    return true;
+  },
+  update: function (userId, doc, fields, modifier) {
+    if (modifier !== '$set') return false;
+    var postChange = fields['name'];
+    if (Boilerplates.findOne({ tags: postChange })) return true;
+    return false;
+  },
+  remove: function (userId, doc) {
+    return (! Boilerplates.finOne({ tags: doc.name }));
+  }
+});
